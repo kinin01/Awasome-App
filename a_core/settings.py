38c260b96c5937c534497c14554460bc686ff581
kinin01 <kiningumbuvi@gmail.com>
2024-03-09 21:56:43 +0000

    debging of database variable second time.

--- a_core/settings.py
+++ a_core/settings.py
@@ -129,7 +129,7 @@ DATABASES = {
 }
 POSTGRES_LOCALLY = False
 if ENVIRONMENT == 'production' or POSTGRES_LOCALLY == True:
-       DATABASES['default'] = dj_database_url.parse(env('DATABASE_UR'))
+       DATABASES['default'] = dj_database_url.parse(env('DATABASE_URL'))
 
 
 # Password validation
